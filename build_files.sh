5d513a5b15dcb05a6021de161e8a2e9af54d57d6
Kaleb <kaleblub@gmail.com>
2024-11-21 16:32:16 -0500

    Tried adding what is needed for Vercel

---
+++ build_files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "--Creating virtual environment--"
+python3 -m venv venv
+echo "--Activating virtual environment--"
+source venv/bin/activate
+echo "--Installing requirements--"
+pip install -r requirements.txt # /production.txt
+/vercel/path0/venv/python3 -m pip install --upgrade pip
+
+echo "--Collecting Static Files--"
+mkdir staticfiles
+python3 manage.py collectstatic --noinput
+
+# Set PYTHONPATH to the project root directory
+export PYTHONPATH="/vercel/path0/$(basename "$(pwd)")"
