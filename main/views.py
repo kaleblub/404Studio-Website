f0ca8ce512bc1ef2afc2a5facdf45a9a5b8e4bcd
Kaleb <kaleblub@gmail.com>
2025-01-22 21:09:51 -0500

    Updated card spacing and layout, implemented templates and other pages to the site, fixed the buttons not being centered in their parent elements.

--- main/views.py
+++ main/views.py
@@ -17,5 +17,17 @@ def home(request):
     # }
     return render(request, 'main/home.html')
 
+def portfolio(request):
+    return render(request, 'main/portfolio.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+def development_packages(request):
+    return render(request, 'main/development_packages.html')
+
+def hosting_packages(request):
+    return render(request, 'main/hosting_packages.html')
+
 def links(request):
     return render(request, 'main/links-page.html')
--- web_404studio/urls.py
+++ web_404studio/urls.py
@@ -6,5 +6,9 @@ from main import views as main_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_views.home, name='home'),
+    path('portfolio/', main_views.portfolio, name='portfolio'),
+    path('development_packages/', main_views.development_packages, name='development_packages'),
+    path('hosting_packages/', main_views.hosting_packages, name='hosting_packages'),
+    path('contact/', main_views.contact, name='contact'),
     path('links/', main_views.links, name='links'),
 ]
