3398629baebce503133da493a92414f328b03f4c
Kaleb <kaleblub@gmail.com>
2025-02-10 02:15:52 -0500

    Added context processor and another translation setting.

--- web_404studio/settings.py
+++ web_404studio/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'web_404studio.context_processors.meeting_links_context',
             ],
         },
     },
@@ -142,6 +143,8 @@ LANGUAGES = [
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
