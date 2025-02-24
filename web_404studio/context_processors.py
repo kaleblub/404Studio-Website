2e2a798cafaca670b1b3aa7d7a9efe4fb493e9ff
Kaleb <kaleblub@gmail.com>
2025-02-23 22:12:00 -0500

    Added corner frames to all of the images on the home page and made the static effect work I think. Also changed the consultation section to a full width image for some site variety.

--- web_404studio/context_processors.py
+++ web_404studio/context_processors.py
@@ -6,6 +6,7 @@ def meeting_links_context(request):
             "dev_premium": "https://cal.com/404-studio-kaleb-humpal/meeting-to-discuss-web-development-package-premium",
             "host_basic": "https://cal.com/404-studio-kaleb-humpal/meeting-to-discuss-hosting-plan-basic",
             "host_standard": "https://cal.com/404-studio-kaleb-humpal/meeting-to-discuss-hosting-plan-standard",
-            "host_premium": "https://calendly.com/kaleblub/meeting-to-discuss-hosting-plan-premium",
+            "host_premium": "https://cal.com/404-studio-kaleb-humpal/hosting-plan-premium-meeting",
+            "30_min": "https://cal.com/404-studio-kaleb-humpal/30-minute-consultation-meeting",
         }
     }
